82cd9b9f14f3d08651c705c04d8420eb725a95f3
HuitziWitzi <63694670+HuitziWitzi@users.noreply.github.com>
2020-04-14 23:53:55 -0400

    Update bam2maya2020.bat

--- bam2maya2020.bat
+++ bam2maya2020.bat
@@ -1,8 +1,3 @@
-:: First Parameter (parameter after calling batch or executable) is %1
-:: Second Parameter is %2
-:: EX bam2maya2020.bat suitB-heads.bam
-::			%1 = suitB-heads.bam
-::			%~f1 = G:/Panda3D/suitB-heads.bam
 set fileName=%1
 set fileName=%fileName:~0,-4%
 bam2egg.exe -o %fileName%.egg %fileName%.bam
